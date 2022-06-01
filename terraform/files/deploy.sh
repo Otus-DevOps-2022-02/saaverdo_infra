863f70c97a1bd943ad1ee92bc7e63d8b89441a69
Serg Braslavets <saaverdo@gmail.com>
2022-06-02 01:25:19 +0300

    Task 6 fixed

--- terraform/files/deploy.sh
+++ terraform/files/deploy.sh
@@ -6,6 +6,6 @@ git clone -b monolith https://github.com/express42/reddit.git $APP_DIR/reddit
 cd $APP_DIR/reddit
 bundle install
 sudo mv /tmp/puma.service /etc/systemd/system/puma.service
-sydo systemctl daemon-reload
+sudo systemctl daemon-reload
 sudo systemctl start puma
 sudo systemctl enable puma
